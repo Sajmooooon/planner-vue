112b34e5273e5289e8a66ebb889b4736e185aa68
simon <sajmoooonsk@gmail.com>
2023-06-20 11:34:37 +0200

    feat: add project view

---
+++ src/views/AddProjectView.vue
@@ -0,0 +1,69 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label>Title:</label>
+    <input type="text" v-model="title" required>
+    <label>Details:</label>
+    <textarea v-model="details" required/>
+    <button>Add Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddProjectView",
+  data(){
+    return{
+      title: "",
+      details: ""
+    }
+  },
+  methods: {
+    handleSubmit(){
+      console.log(this.title, this.details)
+    }
+  }
+}
+</script>
+
+<style scoped>
+form{
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label{
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+input, select, textarea{
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+
+textarea{
+  height: 100%;
+}
+
+form button{
+  display: block;
+  cursor: pointer;
+  background: #2c3e50;
+  border: 0;
+  font-size: 16px;
+  padding: 10px 20px;
+  margin: 20px auto 0;
+  color: aquamarine;
+  border-radius: 20px;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        <SingleProject :project="project" @delete="handleDelete"></SingleProject>
+        <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"></SingleProject>
       </div>
     </div>
     <div v-else>
@@ -26,8 +26,13 @@ export default {
   methods: {
     handleDelete(id){
       this.projects = this.projects.filter(project => project.id!==id)
+    },
+    handleComplete(id){
+      let project = this.projects.find(p => p.id === id)
+      project.complete = !project.complete
     }
   },
+
   mounted() {
     fetch('http://localhost:3000/projects')
         .then(res=>res.json())
